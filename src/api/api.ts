b895083ae933e136bbeee1df2c06c9682a2e5690
Joseph Maramba <josephmaramba50@gmail.com>
2023-03-05 09:22:38 +0300

    split movies and shows

--- src/api/api.ts
+++ src/api/api.ts
@@ -18,9 +18,18 @@ interface Movies {
     vote_count: number;
 }
 
+interface Shows {
+
+}
+
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export const getTrendingMovies = async () => {
     const res = await fetchData<{ results: Movies[] }>(`https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`)
+    return res.results
+}
+
+export const getTrendingShows = async () => {
+    const res = await fetchData<{ results: Shows[] }>(`https://api.themoviedb.org/3/trending/tv/day?api_key=${API_KEY}`)
     return res.results
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,97 @@
-import { Await, defer, useLoaderData } from 'react-router-dom';
-import { getTrendingMovies } from '@/api/api';
 import { Suspense } from 'react';
+import { Await, defer, useLoaderData } from 'react-router-dom';
+import { motion } from 'framer-motion';
+import { getTrendingMovies, getTrendingShows } from '@/api/api';
 import { generateRandomNumber } from '@/utils';
 import '@/styles/root.scss';
 
 export const loader = () => {
-  return defer({ movie: getTrendingMovies() });
+  return defer({ movie: getTrendingMovies(), shows: getTrendingShows() });
 };
 
 export const Root = () => {
   const moviePromise = useLoaderData() as any;
 
+  const categories = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const category = {
+    hidden: {
+      opacity: 0,
+      scale: 0.5,
+      filter: 'blur(10px)',
+    },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      filter: 'blur(0px)',
+    },
+  };
+
   return (
     <section className="home">
       <h1 className="home__title text-center">Movies or Series!</h1>
       <Suspense fallback={<h2>Loading... please wait..</h2>}>
-        <Await
-          resolve={moviePromise.movie}
-          errorElement={<h2>AN error occured</h2>}
+        <motion.div
+          variants={categories}
+          initial="hidden"
+          animate="visible"
+          className="home__cards flex justify-center"
         >
-          {(movies) => {
-            const randomNo = generateRandomNumber(movies.length);
-            const movie = movies[randomNo];
-            return (
-              <div className="home__movie-cards flex justify-center">
-                <article className="home__movie-card flex-1">
+          <Await
+            resolve={moviePromise.movie}
+            errorElement={<h2>AN error occured</h2>}
+          >
+            {(movies) => {
+              const randomNo = generateRandomNumber(movies.length);
+              const movie = movies[randomNo];
+
+              return (
+                <motion.article
+                  variants={category}
+                  className="home__card flex-1"
+                >
                   <img
                     src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
                     alt={movie?.title}
+                    draggable={false}
                   />
-                  {JSON.stringify(movie, null, 2)}
-                </article>
-                <article className="home__movie-card flex-1">
+                </motion.article>
+              );
+            }}
+          </Await>
+          <Await
+            resolve={moviePromise.shows}
+            errorElement={<h2>AN error occured</h2>}
+          >
+            {(movies) => {
+              const randomNo = generateRandomNumber(movies.length);
+              const movie = movies[randomNo];
+
+              return (
+                <motion.article
+                  variants={category}
+                  className="home__card home__card--show flex-1"
+                >
                   <img
                     src={`https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`}
                     alt={movie?.title}
+                    draggable={false}
                   />
-                  {JSON.stringify(movie, null, 2)}
-                </article>
-              </div>
-            );
-          }}
-        </Await>
+                </motion.article>
+              );
+            }}
+          </Await>
+        </motion.div>
       </Suspense>
     </section>
   );
