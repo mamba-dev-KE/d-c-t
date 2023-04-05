67961b1465725f5d3e1dac3b25be0a633951e897
Joseph Maramba <josephmaramba50@gmail.com>
2023-04-05 08:06:09 +0300

    working on header responsiveness

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.scss';
+
 export const Header = () => {
   return (
     <header className="header flex items-center">
       <div className="container flex justify-between items-center">
         <div className="header__logo">
-          <p>Movies & Series</p>
+          <p>Naiflix</p>
         </div>
         <nav className="header__nav">
           <ul className="header__nav-list flex items-center">
@@ -18,8 +19,27 @@ export const Header = () => {
             <li>
               <NavLink to="shows">TV Shows</NavLink>
             </li>
+            <li>
+              <NavLink to="test">Test</NavLink>
+            </li>
           </ul>
         </nav>
+        <div className="header__burger">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 9h16.5m-16.5 6.75h16.5"
+            />
+          </svg>
+        </div>
       </div>
     </header>
   );
