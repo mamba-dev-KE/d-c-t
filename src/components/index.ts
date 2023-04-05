90e2bca66ec75009410e1bf7e40621051de1bf94
Joseph Maramba <josephmaramba50@gmail.com>
2023-04-05 08:06:33 +0300

    components exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,8 @@
 export { Header } from './Header/Header';
-export { Movies } from './Movies/Movies';
+export { MoviesList } from './Movies/MoviesList';
+export { ShowsList } from './Shows/ShowsList'
+export { Layout } from './Layout/Layout'
+export { Card } from './Card/Card'
+export { Error } from './Error/Error'
+export { RootError } from './Error/RootError'
+
