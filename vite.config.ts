3977e450dfbb30e3601dd82a275b3565e6f55baf
Joseph Maramba <josephmaramba50@gmail.com>
2023-04-05 07:32:46 +0300

    dependency cleanup and adding

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import path from 'path';
-import url from 'url';
 
-// const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // alias: {
-    //   '@': path.resolve(__dirname, 'src'),
-    // },
     alias: [{ find: '@', replacement: '/src' }],
   },
 });
